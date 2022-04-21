8ff03c91624b8296c16b59322e86215aa80d37f9
Asim-Coding <muhammadasim19955@gmail.com>
2022-04-21 07:19:40 +0500

    2 files modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -11,5 +11,10 @@ export class ChildComponent implements OnInit {
 
   ngOnInit() {
   }
-
+   employee=[
+     {id:1, name:'Ahmed', Age:22, Salary:55000, Designation:'web developer'},
+     {id:2, name:'Ali', Age:24, Salary:56000, Designation:'game developer'},
+     {id:3, name:'bilal', Age:26, Salary:65000, Designation:'app developer'},
+     {id:4, name:'Jalal', Age:27, Salary:57000, Designation:'ios developer'},
+   ]
 }
